f2d85625b9d6940e5245e22978f9d33b800b261c
mohammad hashom <taylorsuccessor@mohammads-MacBook-Pro.local>
2018-04-07 09:23:39 +0300

    Initial Commit

---
+++ ureed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ureed
+//
+//  Created by mohammad hashom on 4/7/18.
+//  Copyright © 2018 mohammad hashom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ureedTests/ureedTests.swift
@@ -0,0 +1,36 @@
+//
+//  ureedTests.swift
+//  ureedTests
+//
+//  Created by mohammad hashom on 4/7/18.
+//  Copyright © 2018 mohammad hashom. All rights reserved.
+//
+
+import XCTest
+@testable import ureed
+
+class ureedTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
